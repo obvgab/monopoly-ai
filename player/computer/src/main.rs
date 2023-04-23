0e84c842f04c1f79b4974f39c23a64528c6f621f
Obverser <57118333+Obverser@users.noreply.github.com>
2023-04-23 12:19:56 -0700

    Connecting computer client

--- player/computer/src/main.rs
+++ player/computer/src/main.rs
@@ -1,13 +1,46 @@
 use bevy::prelude::*;
-use naia_bevy_client::{ClientConfig, Plugin as ClientPlugin};
-use monai_store::{protocol_builder};
+use naia_bevy_client::{ClientConfig, Plugin as ClientPlugin, Client, transport::webrtc};
+use monai_store::{protocol_builder, Auth};
+use std::env;
 
 mod model;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        println!("-- {{ip:port}} {{auth}} {{name}}");
+        return;
+    }
+
     App::new()
-    .add_plugins(MinimalPlugins)
-    .add_plugin(ClientPlugin::new(ClientConfig::default(), protocol_builder()))
-    .add_startup_system(model::add_stateful)
-    .run();
+        .add_plugins(MinimalPlugins)
+        .add_plugin(ClientPlugin::new(ClientConfig::default(), protocol_builder()))
+        .add_startup_system(model::add_stateful)
+
+        .insert_resource(ClientResources { url: args[1].clone(), code: args[2].clone(), name: args[3].clone() })
+        .add_startup_system(connect_client)
+
+        .run();
+}
+
+#[derive(Resource)]
+pub struct ClientResources {
+    pub url: String,
+    pub code: String,
+    pub name: String
+}
+
+pub fn connect_client(
+    mut info: ResMut<ClientResources>,
+
+    mut client: Client
+) {
+    client.auth(Auth::new(&info.name, &info.code));
+    if !info.url.starts_with("http://") {
+        info.url = "http://".to_string() + info.url.as_str();
+    }
+    println!("Connecting to {} with {} as {}", info.url, info.code, info.name);
+
+    let socket = webrtc::Socket::new(&info.url, client.socket_config());
+    client.connect(socket);
 }
