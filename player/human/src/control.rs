52326d1a1d007cc16e186cce31c71b64ee8b2d09
Obverser <57118333+Obverser@users.noreply.github.com>
2023-04-23 10:57:18 -0700

    Fixing player tracking

--- player/human/src/control.rs
+++ player/human/src/control.rs
@@ -1,6 +1,6 @@
 use bevy::{prelude::*};
 use bevy_egui::{egui, EguiContexts};
-use monai_store::{Auth, transfer::{BoardUpdateChannel, BeginTurn, SendPlayer, StartGame, PlayerActionChannel, BuyOwnable, SellOwnable, Forfeit, EndTurn}, player::{Action, Money, Position}, tile::{Tile, Chance, Corner, ServerSide}};
+use monai_store::{Auth, transfer::{BoardUpdateChannel, BeginTurn, SendPlayer, StartGame, PlayerActionChannel, BuyOwnable, SellOwnable, Forfeit, EndTurn}, player::{Action, Money, Position, ServerPlayer}, tile::{Tile, Chance, Corner, ServerSide}};
 use naia_bevy_client::{Client, transport::webrtc, events::MessageEvents};
 
 #[derive(Resource)]
@@ -20,7 +20,7 @@ pub fn gui( // separate this into multiple functions later
     mut stateful: ResMut<StatefulInformation>,
 
     tiles: Query<(Entity, &mut Tile, Option<&Corner>, Option<&Chance>, &ServerSide), (Without<Money>, Without<Position>)>,
-    tokens: Query<(Entity, &mut Money, &Position), (Without<Tile>, Without<Corner>, Without<Chance>)>,
+    tokens: Query<(Entity, &mut Money, &Position, &ServerPlayer), (Without<Tile>, Without<Corner>, Without<Chance>)>,
 
     mut client: Client,
     mut contexts: EguiContexts
@@ -58,8 +58,17 @@ pub fn gui( // separate this into multiple functions later
         } else if !stateful.started || stateful.entity == 0 {
             ui.label("Waiting for game...");
         } else {
-            // this may cause issues, but the entity bits should be synchronized between client and server in our case
-            let (_, money, position) = tokens.get(Entity::from_bits(stateful.entity)).expect("Could not find active player");
+            let (_entity, money, position, _server_entity) = {
+                let mut last: Option<(Entity, &Money, &Position, &ServerPlayer)> = None;
+                
+                for x in tokens.iter() {
+                    if *x.3.id == stateful.entity {
+                        last = Some(x);
+                    }
+                }
+
+                last.expect("Couldn't find current player from server reference")
+            };
 
             ui.label(format!("Player ID: {:#?}", Entity::from_bits(stateful.entity)));
             ui.label(format!("Money: {}", *money.worth));
